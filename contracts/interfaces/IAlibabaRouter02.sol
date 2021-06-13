0b8564ab047664cf28acdf9171a75505800330d9
alibabaswap <85833509+alibabaswap@users.noreply.github.com>
2021-06-13 16:55:55 +0300

    Update and rename IPancakeRouter02.sol to IAlibabaRouter02.sol

--- contracts/interfaces/IAlibabaRouter02.sol
+++ contracts/interfaces/IAlibabaRouter02.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.6.2;
 
-import './IPancakeRouter01.sol';
+import './IAlibabaRouter01.sol';
 
-interface IPancakeRouter02 is IPancakeRouter01 {
+interface IAlibabaRouter02 is IAlibabaRouter01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
